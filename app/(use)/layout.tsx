9682ff7a9d840ae9316037013a1a5e8515be4baf
hominhk3 <144079318+hominhk3@users.noreply.github.com>
2024-12-26 19:05:22 +0700

    create sidebar

--- app/(use)/layout.tsx
+++ app/(use)/layout.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import style from './style.module.scss'
 import Navbar from '@/components/navbar/Navbar'
+import Sidebar from '@/components/sidebar/Sidebar'
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={style.style_layout}>
+      <Sidebar/>
+      <div className={style.style_Navbar}>
       <Navbar/>
       <main>{children}</main>
+      </div>
     </div>
   )
 }
