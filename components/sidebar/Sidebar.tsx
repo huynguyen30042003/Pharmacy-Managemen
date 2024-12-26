36b9dbe0f53da1b05244b82138e76bf5c67fd8b4
huynguyen30042003 <nguyenkhoanhathuy@gmail.com>
2024-12-26 17:50:14 +0700

    create sidebar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,17 +1,64 @@
-import React from "react";
+"use client"
+import React,{useState} from "react";
 import style from "./Sidebar.module.scss";
 import Image from "next/image";
+import { SidebarItem, sidebarData } from './data';
+
 
 function Sidebar() {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const toggleDetails = (index: number) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
   return (
     <div className={style.sidebar}>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={20}
-        height={20}
-        priority
-      />
+      <div className={style.container}>
+        <Image
+          src="/icon-logo2.svg"
+          alt="colorfilter icon"
+          width={37}
+          height={37}
+          priority
+          className={style.imageLogo}
+        />
+        {sidebarData.map((item: SidebarItem, index: number) => (
+          <div  >
+            <div className={style.select} onClick={() => item.details && toggleDetails(index)}>
+              <div className={style.title}>
+                <Image
+                  src={item.icon}
+                  alt="colorfilter icon"
+                  width={18}
+                  height={18}
+                  priority
+                  className={style.image}
+                />
+                <span>{item.title}</span>
+              </div>
+              {item.details && (
+              <Image
+                src="/drop-down-gray.svg"
+                alt="colorfilter icon"
+                width={16}
+                height={16}
+                priority
+                className={`${openIndex === index ? style.up : style.down} ${style.image}`}
+              />)}
+            </div>
+             {item.details && openIndex === index && (
+              <ul className={style.details}>
+                {item.details.map((detail, idx) => (
+                  <li key={idx} className={style.detailItem}>
+                    {detail.title}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
