c272d99fe320af0f83f9da0930c562f407d29fc3
hominhk3 <144079318+hominhk3@users.noreply.github.com>
2025-01-10 10:28:36 +0700

    MinhHN create customerDue

--- components/path/inputFrom/InputForm.tsx
+++ components/path/inputFrom/InputForm.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import style from './InputForm.module.scss'
-import Input from '../input/Input';
+import React from "react";
+import style from "./InputForm.module.scss";
+import Input from "../input/Input";
 interface InputFormprops {
   title: string;
-  type:string;
-  minWidth?: string
+  type: string;
+  minWidth?: string;
 }
-const InputForm:React.FC<InputFormprops>=({title,type,minWidth})=> {
+const InputForm: React.FC<InputFormprops> = ({ title, type, minWidth }) => {
   return (
-   <div className={style.inputForm}>
-    <label className={style.title} htmlFor="search">{title}</label>
-    <Input minWidth={minWidth} type={type}></Input>
-   </div>
-  )
-}
+    <div className={style.inputForm}>
+      <label className={style.title} htmlFor="search">
+        {title}
+      </label>
+      <Input minWidth={minWidth} type={type}></Input>
+    </div>
+  );
+};
 
-export default InputForm
+export default InputForm;
