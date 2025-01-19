32704e36190de7c9e3de8559f90db2a60a7ec5ed
huynguyen30042003 <nguyenkhoanhathuy@gmail.com>
2025-01-19 20:57:59 +0700

    HuyNK

--- hooks/useLogin.ts
+++ hooks/useLogin.ts
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 export const useLogin = () => {
-    const [email, setEmail] = useState<String>("");
-    const [password, setPassword] = useState<String>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
   
     const login = () => {
       console.log("login");
